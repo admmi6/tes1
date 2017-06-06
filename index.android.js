ee04b29316cf65a28594638a92178b9e70ae158b
karta sasmita <waysasmita@gmail.com>
2017-06-06 19:37:23 +0800

    Add files via upload

---
+++ index.android.js
@@ -0,0 +1,14 @@
+/**
+ KARTA
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import MainApp from './src/MainApp';
+
+AppRegistry.registerComponent('SendDataAPI', () => MainApp);
